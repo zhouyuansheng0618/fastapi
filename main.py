ef40dba75a4b11503f2bd678f19d492c557e7d57
zhouys618 <1767703660@qq.com>
2022-05-19 13:56:47 +0800

    init1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2022/5/19 13:33 
+# @Author : zhouys618@163.com 
+# @File : main.py 
+# @desc:
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,4 +15,8 @@ async def root():
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+@app.get("/hello/{name}")
+async def say_hello111(name: str):
     return {"message": f"Hello {name}"}
